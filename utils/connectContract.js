a7be87d2bfa752d4bb1a7264301c19ff22e918b5
Shireen T <shireentaha@shireens-mbp.lan>
2022-09-01 16:56:38 -0400

    initial commit

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    const contractAddress = "0xf7dED9E8bd8996a7C2b56e3DA799cbf903C5BA7A";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
